ab82a3b80278716acc541bed7a1d3d000126ad7c
EC2 Default User <ec2-user@ip-10-1-0-80.ec2.internal>
2022-08-05 13:15:11 +0000

    terrafrom ec2 instance script without var

---
+++ main.tf
@@ -0,0 +1,162 @@
+provider "aws" {
+ region = "us-east-1"
+ access_key = "AKIAYOSHFORWKJYM623I"
+ secret_key = "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"
+}
+ 
+resource "aws_vpc" "test-prodVPC" {
+  cidr_block       = "10.2.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+ tags = {
+    Name = "test-podVPC"
+	Owner = "Balaram"
+	environment = "Dev"
+	project = "My project"
+    }
+}
+
+resource "aws_internet_gateway" "test-prodIGW" {
+  tags = {
+        Name = "test-prodigw"
+        Owner = "Balaram"
+	Environment = "Dev"
+	Project = "My project"
+    }
+}
+
+resource "aws_internet_gateway_attachment" "test-prodIGW" {
+  internet_gateway_id = aws_internet_gateway.test-prodIGW.id
+  vpc_id              = aws_vpc.test-prodVPC.id
+}
+
+resource "aws_subnet" "test-prod-pub1" {
+    vpc_id = aws_vpc.test-prodVPC.id
+    cidr_block = "10.2.0.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+ tags = {
+        Name = "test-prod-subnet-pub1"
+	Owner = "Balaram"
+	Environment = "Dev"
+	Project = "My project"
+    }
+}
+resource "aws_subnet" "test-prod-pub2" {
+    vpc_id = aws_vpc.test-prodVPC.id
+    cidr_block = "10.2.1.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+  tags = {
+        Name = "test-prodsubnet-pub2"
+        Owner = "Balaram"
+	Environment = "Dev"
+	Project = "My project"
+    }
+}
+
+resource "aws_subnet" "test-prod-priv1" {
+    vpc_id = aws_vpc.test-prodVPC.id
+    cidr_block = "10.2.2.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+  tags = {
+        Name = "test-prodsubnet-priv1"
+	Owner = "Balaram"
+	Environment = "Dev"
+	Project = "My project"
+    }
+	
+}
+resource "aws_subnet" "test-prod-priv2" {
+    vpc_id = aws_vpc.test-prodVPC.id
+    cidr_block = "10.2.3.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+  tags = {
+        Name = "test-prodsubnet-priv2"
+	Owner = "Balaram"
+	Environment = "Dev"
+	Project = "My project"
+    }
+	
+}
+
+resource "aws_route_table" "test-prodRT-pub" {
+    vpc_id = aws_vpc.test-prodVPC.id
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.test-prodIGW.id
+    }
+  tags = {
+        Name = "test-prodRT-pub"
+	Owner = "Balaram"
+	Environment = "Dev"
+	Project = "My project"
+    }
+}
+
+resource "aws_route_table_association" "test-prodRT" {
+    subnet_id = aws_subnet.test-prod-pub1.id
+    route_table_id = aws_route_table.test-prodRT-pub.id
+}
+resource "aws_route_table_association" "test-prodRT1" {
+    subnet_id = aws_subnet.test-prod-pub2.id
+    route_table_id = aws_route_table.test-prodRT-pub.id
+}
+
+resource "aws_route_table" "test-prodRT-priv" {
+    vpc_id = aws_vpc.test-prodVPC.id
+ tags = {
+        Name = "test-prodRT-priv"
+        Owner = "Balaram"
+        Environment = "Dev"
+        Project = "My project"
+    }
+}
+
+resource "aws_route_table_association" "test-prodRT-priv1" {
+    subnet_id = aws_subnet.test-prod-priv1.id
+    route_table_id = aws_route_table.test-prodRT-priv.id
+}
+resource "aws_route_table_association" "test-prodRT-priv2" {
+    subnet_id = aws_subnet.test-prod-priv2.id
+    route_table_id = aws_route_table.test-prodRT-priv.id
+}
+
+resource "aws_security_group" "test-prod-SG" {
+  description = "aws_vpc.Allow all inbound traffic"
+  vpc_id      =  aws_vpc.test-prodVPC.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_instance" "Webserver1" {
+      ami = "ami-0cff7528ff583bf9a"
+	  availability_zone = "us-east-1a"
+	  instance_type = "t2.micro"
+	  vpc_security_group_ids = ["${aws_security_group.test-prod-SG.id}"]
+	  key_name = "RamAWS"
+	  subnet_id = aws_subnet.test-prod-pub1.id
+	  associate_public_ip_address = true
+          user_data = "${file("nginx.txt")}"
+      tags = {
+	        Name = "Webserver1"
+	        Owner = "Balaram"
+	        Environment = "Dev"
+	        Project = "My project"
+		}
+	}
